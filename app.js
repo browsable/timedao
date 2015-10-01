f1a63609bae49d89b3b8d7bdb30e732ae2379228
browsable <hernia@koreatech.ac.kr>
2015-10-01 20:47:37 +0900

    init

--- app.js
+++ app.js
@@ -64,3 +64,5 @@ module.exports = app;
 var server =app.listen(app.get('port'), function () {
     console.log('Express server listening on port' + server.address().port);
 });
+
+module.exports = app;
