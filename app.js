8780274fe99d18589a30f25b995f4e524a45be5a
browsable <hernia@koreatech.ac.kr>
2015-10-02 15:54:48 +0900

    android nodejs connection

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('express-method-override')());
+app.use(require('method-override')());
 app.use(validator);
 app.use('/', routes);
 app.use('/users', users);
