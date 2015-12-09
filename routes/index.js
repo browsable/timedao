58c80c044989a3db71928c051d8eac41f223126d
browsable <hernia@koreatech.ac.kr>
2015-12-09 15:45:36 +0900

    타이틀 수정

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TimeDAO' });
+  res.render('index', { title: 'TimeDAO Test' });
 });
 
 module.exports = router;
